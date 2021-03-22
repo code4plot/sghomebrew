aea468984990fdd28b21abfc081875313be0a885
code4plot <mail4plot@gmail.com>
2021-03-22 13:17:48 +0800

    RDS
    Connect shiny app to AWS RDS (MySQL)

---
+++ kegland/kegORDERator/utils/data_handler.R
@@ -0,0 +1,61 @@
+library(RMySQL)
+source("utils/parameters.R")
+
+
+options(mysql = db_cred)
+
+db_connect <- function(){
+  db <- dbConnect(MySQL(), 
+                  dbname = databaseName, 
+                  host = options()$mysql$host,
+                  port = options()$mysql$port,
+                  user = options()$mysql$user,
+                  password = options()$mysql$password
+  )
+  return(db)
+}
+
+saveData <- function(data, tbl){
+  db <- db_connect()
+  query <- sprintf(
+    "INSERT INTO %s (%s) VALUES ('%s')",
+    tbl,
+    paste(names(data), collapse = ", "),
+    paste(data, collapse = ", ")
+  )
+  dbGetQuery(db, query)
+  dbDisconnect(db)
+}
+
+loadUsers <- function(tbl = table_users){
+  db <- db_connect()
+  query <- sprintf("SELECT uname FROM %s",
+                   tbl)
+  data <- dbGetQuery(db, query)
+  dbDisconnect(db)
+  return(data)
+}
+
+loadOrders <- function(tbl = table_orders, name){
+  db <- db_connect()
+  query <- sprintf("SELECT name, klid, SUM(q) as q FROM %s
+                   WHERE name = %s
+                   GROUP BY klid", tbl, name)
+  data <- dbGetQuery(db, query)
+  dbDisconnect(db)
+  return(data)
+}
+
+loadAllOrders <- function(tbl = table_orders){
+  db <- db_connect()
+  query <- sprintf("SELECT b.klid, SUM(b.q) as q FROM 
+                  (SELECT a.name, a.klid, SUM(a.q) as q FROM %s AS a
+                   GROUP BY a.name, a.klid) AS b
+                   WHERE b.q > 0
+                   GROUP BY b.klid")
+  data <- dbGetQuery(db, query)
+  dbDisconnect(db)
+  return(data)
+}
+
+
---
+++ kegland/kegORDERator/utils/initialize_db.R
@@ -0,0 +1,35 @@
+library(RMySQL)
+source("utils/parameters.R")
+source("utils/data_handler.R")
+
+
+
+options(mysql = db_cred)
+
+db <- db_connect()
+
+#create users tbl
+query_users <- "CREATE TABLE users
+(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  uname varchar(90) NOT NULL,
+  paynow INT NOT NULL
+)
+"
+
+#create orders tbl
+query_orders <- "CREATE TABLE orders
+(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  name varchar(90) NOT NULL,
+  klid varchar(10) NOT NULL,
+  q INT NOT NULL
+)
+"
+dbGetQuery(db, query_users)
+
+dbGetQuery(db, query_orders)
+
+dbDisconnect(db)
